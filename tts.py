71364e8122366881b50e6588caa27ab492ffa986
Arnau Mora <arnyminerz@proton.me>
2023-11-27 19:02:18 +0100

    Script

--- tts.py
+++ tts.py
@@ -55,18 +55,23 @@ synthesis_input = texttospeech.SynthesisInput(ssml=ssml)
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
 voice = texttospeech.VoiceSelectionParams(
-    language_code=language_code, ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    language_code=language_code,
+    name="en-US-Neural2-C"
 )
 
 # Select the type of audio file you want returned
 audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
+    audio_encoding=texttospeech.AudioEncoding.MP3,
+    speaking_rate=1,
+    pitch=0
 )
 
 # Perform the text-to-speech request on the text input with the selected
 # voice parameters and audio file type
 response = client.synthesize_speech(
-    input=synthesis_input, voice=voice, audio_config=audio_config
+    input=synthesis_input,
+    voice=voice,
+    audio_config=audio_config
 )
 
 # The response's audio_content is binary.
